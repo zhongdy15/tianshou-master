909b53c0c892abc38933477f204efd7f36f78b1b
zhongdy15 <zhongdy15@foxmail.com>
2022-05-05 15:09:51 +0800

    确定性环境测试

--- test/discrete/test_ppo.py
+++ test/discrete/test_ppo.py
@@ -12,7 +12,7 @@ package = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__fil
 # print(package)
 # print(sys.path)
 sys.path.insert(0, package)
-os.environ['CUDA_VISIBLE_DEVICES'] = '2,3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,2,3,4'
 # print(sys.path)
 # import tianshou
 # print(tianshou.utils.__path__)
