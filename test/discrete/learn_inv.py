2254670622a8e63910106a5ff90754ed16005afd
zhongdy15 <zhongdy15@foxmail.com>
2022-08-16 02:56:19 +0800

    Asterix

--- test/discrete/learn_inv.py
+++ test/discrete/learn_inv.py
@@ -11,7 +11,7 @@ torch.set_num_threads(16)
 import sys
 package = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.insert(0, package)
-os.environ['CUDA_VISIBLE_DEVICES'] = '5'
+os.environ['CUDA_VISIBLE_DEVICES'] = '6'
 from tianshou.data import Collector, VectorReplayBuffer, ReplayBuffer
 import torch.nn as nn
 
@@ -38,7 +38,7 @@ class InverseModel(nn.Module):
             nn.Flatten(),
             layer_init(nn.Linear(32 * 4 *3, 256)),
             nn.ReLU(), )
-        self.prob_predict = layer_init(nn.Linear(256, 7), std=0.01)
+        self.prob_predict = layer_init(nn.Linear(256, 9), std=0.01)
 
     def forward(self, x):
         bchw_x = x.permute((0, 3, 1, 2))
@@ -51,15 +51,15 @@ class InverseModel(nn.Module):
 if __name__ == '__main__':
 
     buffer_dir = os.path.join("/media/yyq/data/zdy",
-                              "log/ActionBudget_ALE/Assault-v5/ppo",
-                              "maskTrue_actionbudget200_seed0_2022-08-15-16-47-12")
+                              "log/ActionBudget_ALE/Asterix-v5/ppo",
+                              "maskTrue_actionbudget400_seed0_2022-08-15-16-45-41")
     # 之前较好的数据集："maskFalse_actionbudget100_seed0_2022-07-18-11-24-44")
     # buffer_dir = "D:\zhongdy\\research\\tianshou-master\\remote_log\垃圾"
     buffer_list = os.listdir(buffer_dir)
 
     log_name = "inv_" + time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
 
-    log_path = os.path.join('log', 'ActionBudget_ALE/Assault-v5', 'ppo', log_name)
+    log_path = os.path.join('log', 'ActionBudget_ALE/Asterix-v5', 'ppo', log_name)
     writer = SummaryWriter(log_path)
     inversemodel = InverseModel().to(device)
     optimizer_inversemodel = torch.optim.Adam(inversemodel.parameters(), lr=1e-4, eps=1e-5)
